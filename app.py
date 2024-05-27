213e3b69287668e1f6d4ba58b73211b75ecfb119
MariusSuessmilch <47320006+MariusSuessmilch@users.noreply.github.com>
2024-05-27 23:26:24 +0200

    score, hidden text

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import random
+import time
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 
 openai_api_key = st.secrets["openai_key"]
 llm = ChatOpenAI(api_key=openai_api_key, model="gpt-4o")
 random_number = random.randint(0,1)
+
+if "score" not in st.session_state:
+    st.session_state.score = 0
 
 @st.experimental_fragment
 def decision_fragment():
@@ -14,14 +18,24 @@ def decision_fragment():
         st.markdown("Von wem stammt der linke Text?")
     elif decision == "Mensch" and random_number==0:
         st.markdown("**Richtig!**")
+        time.sleep(1)
+        st.session_state["score"] += 1
+        st.rerun()
+    elif decision == "KI" and random_number == 1:
+        st.markdown("**Richtig!**")
+        time.sleep(1)
+        st.session_state["score"] += 1
+        time.sleep(1)
+        st.rerun()
     else: 
         st.markdown("**Das stimmt leider nicht!**")
+        st.rerun()
 
 st.title("Turing Test")
 st.subheader("Mensch vs KI - erkennst du den Unterschied?")
 
 question = st.text_input("Frage/Prompt")
-human_text = st.text_area("Deine Antwort:")
+human_text = st.text_input("Deine Antwort:", type="password")
 prompt = f"Du beantwortest Fragen und Prompts in sehr menschenähnlichem Stil. Halte dich eher kurz und schreibe nicht zu formal, sondern wie man in einer Unterhaltung sprechen würde. Antworte auf diesen prompt: {question}"
 if st.button("Generiere Antwort"):
     with st.spinner("Antworten werden bereitgestellt..."):
@@ -39,5 +53,5 @@ if st.button("Generiere Antwort"):
                 st.info(human_text)
             else:
                 st.info(ai_text)
-            
+st.markdown(f"**Deine Punktzahl ist: {st.session_state['score']}**")    
         
